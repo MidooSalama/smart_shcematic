4b22591dce39916e0fdffc84575b26292ef521b7
salama <eng.mahmoud.a.salama@gmail.com>
2020-09-13 13:50:12 +0200

    added Documentation

--- main.py
+++ main.py
@@ -10,25 +10,27 @@ import schematicDrawing
 
 
 class MplCanvas(FigureCanvasQTAgg):
+    # the Canvas class to load the figure of the schematic
     def __init__(self):
         self.schem = schematicDrawing.Schematic()
         fig = self.schem.figure.fig
-        self.axes = fig.add_subplot(111)
         super(MplCanvas, self).__init__(fig)
 
 
 
 class SchematicApp(QtWidgets.QMainWindow):
+    # the main class to build the GUI window
     def __init__(self):
         super().__init__()
+        # build and initialize the geometry
         self.setWindowTitle("Schematic")
         self.setGeometry(100, 200, 650, 600)
 
-
+        # create the two main containers of the GUI
         self.canvas = MplCanvas()
         self.createInputGroupBox()
 
-
+        # organize the GUI interface
         grid = QtWidgets.QGridLayout()
         grid.addWidget(self.canvas, 0, 0)
         grid.addWidget(self.inputGroupBox, 1, 0)
@@ -36,18 +38,20 @@ class SchematicApp(QtWidgets.QMainWindow):
         widget.setLayout(grid)
         self.setCentralWidget(widget)
 
+        # connect the signals and slots
         self.comboBox.activated.connect(self.reColorElement)
 
     def reColorElement(self):
+        # redraw the schematic to change the color of the selected elements
         element = self.comboBox.currentText()
-        self.canvas.axes.cla()
         self.canvas.schem.redraw(element)
-        self.canvas.figure=self.canvas.schem.figure.fig
+        self.canvas.figure = self.canvas.schem.figure.fig
         self.canvas.draw()
 
 
 
     def createInputGroupBox(self):
+        # this function build and organize the container of the input data
         self.inputGroupBox = QtWidgets.QGroupBox('choose element name', self)
         grid = QtWidgets.QGridLayout()
 
@@ -65,6 +69,7 @@ class SchematicApp(QtWidgets.QMainWindow):
 
 
 if __name__ == '__main__':
+    # start the main procedure of executing the application
     app = QtWidgets.QApplication(sys.argv)
     myApp = SchematicApp()
     myApp.show()
--- schematicDrawing.py
+++ schematicDrawing.py
@@ -4,6 +4,7 @@ from schemdraw.segments import *
 
 
 class MosfetN(elm.Element):
+    # This class build a custom element (Mosfet of N type)for the SchemDraw
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         factor = 0.5
@@ -18,7 +19,9 @@ class MosfetN(elm.Element):
         self.anchors['drain'] = [0.5, 1]
         self.anchors['source'] = [0.5, -1]
 
+
 class MosfetP(elm.Element):
+    # This class build a custom element (Mosfet of P type)for the SchemDraw
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         factor = 0.5
@@ -34,83 +37,92 @@ class MosfetP(elm.Element):
         self.anchors['source'] = [0.5, 1]
 
 class Schematic():
+    # This class Draw the Schematic with its basic building blocks
     def __init__(self):
         self.redraw()
     
-    def redraw(self, elelmentName=None):
+    def redraw(self, elementName=None):
+        # This function takes the element name to be colored in red
+        # Input:
+        #   - elementName: the name of the element that need to be High lighted
+
         self.d = schemdraw.Drawing()
         VoltageSource = self.d.add(elm.Vdd(label='V$_{DD}$'))
-        
+
+
         # the right side branch
         self.d.add(elm.Line(l=7)) # the right side Vdd
         self.d.add(elm.Arrow(d='down',l=1, botlabel='I$_{bias}$'))
-        if(elelmentName == 'M5'):
+        if(elementName == 'M5'):
             M5b = self.d.add(MosfetP(d='right', anchor='source', color='red', rgtlabel='M5b'))  # M5b
         else:
             M5b = self.d.add(MosfetP(d='right', anchor='source', rgtlabel='M5b')) # M5b
         self.d.add(elm.Line(d='down', at=M5b.drain, l=1))
-        if(elelmentName == 'M4'):
+        if(elementName == 'M4'):
             M4b = self.d.add(MosfetP(d='right', anchor='source', color='red', rgtlabel='M4b'))  # M4b
         else:
             M4b = self.d.add(MosfetP(d='right', anchor='source', rgtlabel='M4b')) # M4b
         self.d.add(elm.Dot(open=True, at=M4b.gate, lftlabel='V$_{cascp}$'))
         M4bDotM3b = self.d.add(elm.LineDot(d='down', at=M4b.drain, l=1))
         self.d.add(elm.Line(d='down', l=1))
-        if(elelmentName == 'M3'):
+        if(elementName == 'M3'):
             M3b = self.d.add(MosfetN(d='right', anchor='drain', color='red', rgtlabel='M3b'))
         else:
             M3b = self.d.add(MosfetN(d='right', anchor='drain', rgtlabel='M3b'))
         self.d.add(elm.Dot(open=True, at=M3b.gate, lftlabel='V$_{cascn}$'))
         M3bDotM2b = self.d.add(elm.LineDot(d='down', at=M3b.source, l=1))
         self.d.add(elm.Line(d='down', l=1))
-        if(elelmentName == 'M2'):
+        if(elementName == 'M2'):
             M2b = self.d.add(MosfetN(d='right', anchor='drain', color='red', rgtlabel='M2b'))
         else:
             M2b = self.d.add(MosfetN(d='right', anchor='drain', rgtlabel='M2b'))
         self.d.add(elm.Line(d='down', at=M2b.source, l=1))
         self.d.add(elm.Ground())
-        
+
+
         # the left side branch
         self.d.add(elm.Line(d='left',l=7,at=VoltageSource.end)) # the left side Vdd
         self.d.add(elm.Arrow(d='down',l=1, toplabel='I$_{bias}$'))
-        if(elelmentName == 'M5'):
+        if(elementName == 'M5'):
             M5a = self.d.add(MosfetP(d='right', reverse=True, anchor='source', color='red', lftlabel='M5a'))  # M5a
         else:
             M5a = self.d.add(MosfetP(d='right',reverse=True, anchor='source', lftlabel='M5a')) # M5a
         self.d.add(elm.Line(d='down', at=M5a.drain, l=1))
-        if(elelmentName == 'M4'):
+        if(elementName == 'M4'):
             M4a = self.d.add(MosfetP(d='right', reverse=True, anchor='source', color='red', lftlabel='M4a'))  # M4a
         else:
             M4a = self.d.add(MosfetP(d='right',reverse=True, anchor='source', lftlabel='M4a')) # M4a
         self.d.add(elm.Dot(open=True, at=M4a.gate, rgtlabel='V$_{cascp}$'))
         M4aDotM3a = self.d.add(elm.LineDot(d='down', at=M4a.drain, l=1))
         self.d.add(elm.Line(d='down', l=1))
-        if(elelmentName == 'M3'):
+        if(elementName == 'M3'):
             M3a = self.d.add(MosfetN(d='right', reverse=True, anchor='drain', color='red', lftlabel='M3a')) # M3a
         else:
             M3a = self.d.add(MosfetN(d='right',reverse=True, anchor='drain', lftlabel='M3a')) # M3a
         self.d.add(elm.Dot(open=True, at=M3a.gate, rgtlabel='V$_{cascn}$'))
         M3aDotM2a = self.d.add(elm.LineDot(d='down', at=M3a.source, l=1))
         self.d.add(elm.Line(d='down', l=1))
-        if(elelmentName == 'M2'):
+        if(elementName == 'M2'):
             M2a = self.d.add(MosfetN(d='right', reverse=True, anchor='drain', color='red', lftlabel='M2a')) # M2a
         else:
             M2a = self.d.add(MosfetN(d='right',reverse=True, anchor='drain', lftlabel='M2a')) # M2a
         self.d.add(elm.Line(d='down', at=M2a.source, l=1))
         self.d.add(elm.Ground())
-        
+
+
         # the middle branch
         self.d.add(elm.Arrow(d='down', at=VoltageSource.end, botlabel='2I$_{bias}$'))
-        if(elelmentName == 'M6'):
+        if(elementName == 'M6'):
             M6 = self.d.add(MosfetP(d='right', anchor='source', color='red', rgtlabel='M6')) # M6
         else:
             M6 = self.d.add(MosfetP(d='right', anchor='source', rgtlabel='M6')) # M6
         Vcmfb = self.d.add(elm.Dot(open=True, at=M6.gate, lftlabel='V$_{cmfb}$'))
         M6DotDrain = self.d.add(elm.LineDot(d='down', at=M6.drain, l=1))
+
         # right of the middle branch
         self.d.add(elm.Line(d='right', l=2))
         self.d.add(elm.Arrow(d='down', l=1, botlabel='I$_{bias}$'))
-        if(elelmentName == 'M1'):
+        if(elementName == 'M1'):
             M1b = self.d.add(MosfetP(d='right', reverse=True, anchor='source', color='red', lftlabel='M1b')) # M1b
         else:
             M1b = self.d.add(MosfetP(d='right', reverse=True, anchor='source', lftlabel='M1b')) # M1b
@@ -121,7 +133,7 @@ class Schematic():
         # left of the middle branch
         self.d.add(elm.Line(d='left', xy=M6DotDrain.end, l=2))
         self.d.add(elm.Arrow(d='down', l=1, toplabel='I$_{bias}$'))
-        if(elelmentName == 'M1'):
+        if(elementName == 'M1'):
             M1a = self.d.add(MosfetP(d='right', anchor='source', color='red', rgtlabel='M1a')) # M1a
         else:
             M1a = self.d.add(MosfetP(d='right', anchor='source', rgtlabel='M1a')) # M1a
@@ -144,6 +156,7 @@ class Schematic():
         self.d.add(elm.LineDot(d='right', at=M4bDotM3b.end, rgtlabel='V$_{out+}$'))
         self.d.add(elm.LineDot(d='left', at=M4aDotM3a.end, lftlabel='V$_{out-}$'))
 
+        # draw as a figure to handle it to the GUI canvas
         self.figure = self.d.draw(show=False)
 
 
